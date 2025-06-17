7655cd165a5f6daccbbf6e5408330764d5c0d3be
momin210103 <momincse13@gmail.com>
2025-06-17 16:01:53 +0600

    Add custom Constrain Routing

--- RoutingBasics/Program.cs
+++ RoutingBasics/Program.cs
@@ -1,4 +1,10 @@
+using RoutingBasics.CustomConstraints;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRouting(options =>
+{
+    options.ConstraintMap.Add("months", typeof(MonthsCustomConstraint));
+});
 var app = builder.Build();
 
 app.UseRouting();
@@ -8,6 +14,16 @@ app.UseEndpoints(endpoints =>
     endpoints.Map("profile/name", async context =>
     {
         context.Response.WriteAsync("Hello Momin!");
+
+    });
+    endpoints.Map("sales-report/{year:int:min(19000)}/{month:months}", async context =>
+    {
+        int year = Convert.ToInt32(context.Request.RouteValues["year"]);
+        string? month = Convert.ToString(context.Request.RouteValues["month"]);
+        if(month=="apr" || month=="jul" || month == "oct" || month == "jan")
+        {
+            await context.Response.WriteAsync($"Sales Report for {month} {year}");
+        }
 
     });
 });
