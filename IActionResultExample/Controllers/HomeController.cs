0adb57b3f378d358afdf2454c35d2949ef1ac049
momin210103 <momincse13@gmail.com>
2025-06-19 11:55:48 +0600

    Shortcurt Status code add

--- IActionResultExample/Controllers/HomeController.cs
+++ IActionResultExample/Controllers/HomeController.cs
@@ -10,30 +10,35 @@ namespace IActionResultExample.Controllers
         {
             if (!Request.Query.ContainsKey("bookid"))
             {
-                Response.StatusCode = 400; // Bad Request
-                return Content("Book ID is not supllied.");
+                //Response.StatusCode = 400; // Bad Request
+                //return Content("Book ID is not supllied.");
+                return BadRequest("Book ID is not supplied.");
             }
             if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
             {
-                Response.StatusCode = 400; // Bad Request
-                return Content("Book ID is empty.");
+                //Response.StatusCode = 400; // Bad Request
+                //return Content("Book ID is empty.");
+                return BadRequest("Book ID is empty.");
             }
             int bookId = Convert.ToInt16(ControllerContext.HttpContext.Request.Query["bookid"]);
             if (bookId <= 0)
             {
-                Response.StatusCode = 400; // Bad Request
-                return Content("Book ID cannot be less than or equal to zero.");
+                //Response.StatusCode = 400; // Bad Request
+                //return Content("Book ID cannot be less than or equal to zero.");
+                return BadRequest("Book ID cannot be less than or equal to zero.");
             }
             if (bookId >1000)
             {
-                Response.StatusCode = 400; // Bad Request
-                return Content("Book ID cannot be greater than 1000.");
+                //Response.StatusCode = 400; // Bad Request
+                //return Content("Book ID cannot be greater than 1000.");
+                return NotFound("Book ID cannot be greater than 1000.");
             }
             //isloggind should be true
             if(Convert.ToBoolean(Request.Query["isloggedin"]) == false)
             {
-                Response.StatusCode = 401; // Unauthorized
-                return Content("You are not authorized to view this book.");
+                //Response.StatusCode = 401; // Unauthorized
+                //return Content("You are not authorized to view this book.");
+                return Unauthorized("You are not authorized to view this book.");
             }
             return File("/sample.pdf", "application/pdf");
         }
