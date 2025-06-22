817116a20d77bf586b3ac69e7f7c2b544d75b1f4
momin210103 <momincse13@gmail.com>
2025-06-22 20:02:19 +0600

    Add ASP.NET Core project with controllers and examples
    Introduced a new ASP.NET Core project named `ModelBinding`.
    Added configuration files (`appsettings.json`, `appsettings.Development.json`, `launchSettings.json`),
    a solution file (`ModelBinding.sln`), and a project file (`ModelBinding.csproj`).
    
    Implemented a `HomeController` to handle bookstore-related requests,
    with support for query parameters and input validation. Added a `Book` model
    class to represent data passed to the controller.
    
    Included extensive documentation on controllers, their responsibilities,
    and various action result types (`ContentResult`, `JsonResult`, `File Results`,
    `Status Code Results`, `Redirect Results`) with examples.
    
    Added a generated PDF file (`ASP_NET_Core_Controller_Assignments_Interview.pdf`)
    and metadata. Configured the application to use controllers, routing, and
    development-specific settings.

---
+++ ModelBinding/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Mvc;
+using ModelBinding.Models;
+
+namespace ModelBinding.Controllers
+{
+    public class HomeController: Controller
+    {
+        [Route("bookstore")]
+        public IActionResult Index(int? bookId, bool? isloggedin, Book book)
+        {
+            if(bookId.HasValue == false)
+            {
+                return BadRequest("Book ID is required.");
+            }
+            if(bookId < 1)
+            {
+                return BadRequest("Book ID must be greater than 0.");
+            }
+            if(isloggedin == false)
+            {
+                return BadRequest("Login status is required.");
+            }
+            return Content($"Book ID:{bookId}, Book Object:{book}");
+        }
+    }
+}
---
+++ ModelBinding/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace ModelBinding.Models
+{
+    public class Book
+    {
+        public int? BookId { get; set; }
+        public string Character { get; set; }
+        public string? Author { get; set; }
+        public override string ToString()
+        {
+            return $"Character: {Character},Book ID: {BookId}, Author: {Author}";
+        }
+    }
+}
