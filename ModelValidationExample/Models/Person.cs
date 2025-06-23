8f570d6e6af6b0849137756584d554186e686ee0
momin210103 <momincse13@gmail.com>
2025-06-23 16:16:30 +0600

    ModelValidation Attribute Add

--- ModelValidationExample/Models/Person.cs
+++ ModelValidationExample/Models/Person.cs
@@ -1,16 +1,35 @@
-﻿using System.ComponentModel.DataAnnotations; // namespace for Attribute
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation; // namespace for Attribute
 namespace ModelValidationExample.Models
 {
     public class Person
     {
-        [Required] //attribute apply for data validations
+        [Required (ErrorMessage = " {0} can't be empty or null")] //attribute apply for data validations
+        [Display(Name = "Person Name")] // Display attribute for custom display name
+        [StringLength(40,MinimumLength = 5, ErrorMessage = "{0} should between {2} and {1} characters")]
+        [RegularExpression("^[A-Za-z .]*$", ErrorMessage = "{0} should contain only alphabets,space and dot(.)")]
         public string? PersonName { get;set; }
+
+        [EmailAddress(ErrorMessage = "{0} Invalid ")]
         public string? Email { get; set; }
-        [Required (ErrorMessage = "Phone must be need")]
+
+
+        [Phone(ErrorMessage = "{0} should be 11 digits")]
+        //[ValidateNever]
         public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "{0} can't be blank")]
         public string? Password { get; set; }
+
+
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [Compare("Password", ErrorMessage = "{0} and {1} do not match Re-enter the password")]
+        [Display(Name = "Confirm Password")]
         public string? ConfirmPassword { get; set; }
-        public string? Price { get; set; }
+
+
+        [Range(0,999.99, ErrorMessage = "{0} should be between {1}Tk and {2}Tk")]
+        public Double? Price { get; set; }
 
         public override string ToString()
         {
