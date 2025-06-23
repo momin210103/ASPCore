e0bc3832f14142d158f79df17ff169b7a98ce915
momin210103 <momincse13@gmail.com>
2025-06-23 15:29:20 +0600

    Initial project setup with ASP.NET Core and model validation
    Added configuration files (`appsettings.json`, `appsettings.Development.json`) for logging and allowed hosts.
    Created `.csproj` targeting .NET 9.0 with nullable and implicit `using` enabled.
    Added solution file (`ModelValidationExample.sln`) with Debug/Release configs.
    Implemented `Program.cs` to configure and run the ASP.NET Core app.
    Added `HomeController` with `/register` route for model validation.
    Defined `Person` model with validation attributes and custom `ToString`.
    Configured development environment via `launchSettings.json`.

---
+++ ModelValidationExample/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidationExample.Models;
+
+namespace ModelValidationExample.Controllers
+{
+    [Route("register")]
+    public class HomeController: Controller
+    {
+        public IActionResult Index(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                //List<string> errorsList = new List<string>();
+                //foreach (var value in ModelState.Values)
+                //{
+                //    foreach (var error in value.Errors)
+                //    {
+                //        errorsList.Add(error.ErrorMessage);
+                //    }
+                //}
+                //string errors = string.Join("\n", errorsList);
+                //int totalErrors = errorsList.Count;
+
+                // Another Way 
+                string errors = string.Join("\n",ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage));
+                int totalErrors = ModelState.Values.Sum(value => value.Errors.Count);
+
+                return BadRequest(errors);
+            }
+            return Content($"{person}");
+        }
+    }
+}
---
+++ ModelValidationExample/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations; // namespace for Attribute
+namespace ModelValidationExample.Models
+{
+    public class Person
+    {
+        [Required] //attribute apply for data validations
+        public string? PersonName { get;set; }
+        public string? Email { get; set; }
+        [Required (ErrorMessage = "Phone must be need")]
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public string? Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person Object - PersonName:{PersonName}, Email:{Email},Phone:{Phone}," +
+                $"Password:{Password},Confirm Password:{ConfirmPassword},Price:{Price}";
+        }
+
+    }
+}
