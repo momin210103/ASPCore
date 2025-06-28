3222f3d3a4ae78fbb45ef5391d355a2c8dd12beb
momin210103 <momincse13@gmail.com>
2025-06-28 16:56:35 +0600

    razor view

--- ViewExample/Controllers/HomeController.cs
+++ ViewExample/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ namespace ViewExample.Controllers
     public class HomeController: Controller
     {
         [Route("home")]
+        [Route("/")]
         public IActionResult Index()
         {
             return View();//Views/Home/Index.cshtml
---
+++ ViewExample/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace ViewExample.Models
+{
+    public class Person
+    {
+        public string? name { get; set; }
+        public int? age { get; set; }
+    }
+}
