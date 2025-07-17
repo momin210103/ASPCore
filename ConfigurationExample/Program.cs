1b00e214e44312936bbbaf20483f5f22ee536e5e
momin210103 <momincse13@gmail.com>
2025-07-18 02:00:01 +0600

    Add MyOwnConfig.json for weather API configuration
    Updated `Program.cs` to load configuration from `MyOwnConfig.json`, allowing for optional and reloadable settings. Removed the `weatherapi` section from `appsettings.Development.json`, which eliminates the use of development credentials. Introduced `MyOwnConfig.json` containing `ClientId` and `ClientSecret` values, marking a shift in sensitive information management.

--- ConfigurationExample/Program.cs
+++ ConfigurationExample/Program.cs
@@ -8,6 +8,12 @@ builder.Services.AddControllersWithViews();
 builder.Services.Configure<WeatherApiOptions>(builder.Configuration.GetSection("weatherapi"));
 var app = builder.Build();
 
+// load  MyOwnConfig.json
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile("MyOwnConfig.json", optional: true, reloadOnChange: true);
+});
+
 app.UseStaticFiles();
 app.UseRouting();
 
